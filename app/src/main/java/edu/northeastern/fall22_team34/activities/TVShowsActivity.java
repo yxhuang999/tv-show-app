b9596f8ff0221e80530d189183dbe71c7b5a218f
Yixuan Huang <huang.yixua@northeastern.edu>
2022-10-22 16:12:45 -0400

    RecyclerView integration

--- app/src/main/java/edu/northeastern/fall22_team34/activities/TVShowsActivity.java
+++ app/src/main/java/edu/northeastern/fall22_team34/activities/TVShowsActivity.java
@@ -1,33 +1,93 @@
 package edu.northeastern.fall22_team34.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.northeastern.fall22_team34.R;
+import edu.northeastern.fall22_team34.adapters.TVShowsAdapter;
+import edu.northeastern.fall22_team34.databinding.ActivityTvshowsBinding;
+import edu.northeastern.fall22_team34.models.TVShow;
 import edu.northeastern.fall22_team34.viewmodels.MostPopularTVShowsViewModel;
 
 public class TVShowsActivity extends AppCompatActivity {
 
+    private ActivityTvshowsBinding activityTvshowsBinding;
     private MostPopularTVShowsViewModel viewModel;
+    private List<TVShow> tvShows = new ArrayList<>();
+    private TVShowsAdapter tvShowsAdapter;
+    private int currentPage = 1;
+    private int totalAvailablePages = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tvshows);
+        activityTvshowsBinding = DataBindingUtil.setContentView(this, R.layout.activity_tvshows);
+        //setContentView(R.layout.activity_tvshows);
 
+        doInitialization();
+    }
+
+    private void doInitialization() {
+        activityTvshowsBinding.tvShowRecyclerView.setHasFixedSize(true);
         viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        tvShowsAdapter = new TVShowsAdapter(tvShows);
+        activityTvshowsBinding.tvShowRecyclerView.setAdapter(tvShowsAdapter);
+
+        activityTvshowsBinding.tvShowRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (!activityTvshowsBinding.tvShowRecyclerView.canScrollVertically(1)) {
+                    if (currentPage <= totalAvailablePages) {
+                        currentPage += 1;
+                        getMostPopularTVShows();
+                    }
+                }
+            }
+        });
 
         getMostPopularTVShows();
     }
 
     private void getMostPopularTVShows() {
-        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->
-                Toast.makeText(getApplicationContext(),
-                        "Total Pages: " + mostPopularTVShowsResponse.getTotalPages(),
-                        Toast.LENGTH_SHORT).show()
-        );
+        toggleLoading();
+
+        viewModel.getMostPopularTVShows(currentPage).observe(this, mostPopularTVShowsResponse -> {
+            toggleLoading();
+            if (mostPopularTVShowsResponse != null) {
+                totalAvailablePages = mostPopularTVShowsResponse.getTotalPages();
+
+                if (mostPopularTVShowsResponse.getTvShows() != null) {
+                    int oldCount = tvShows.size();
+                    tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
+                    tvShowsAdapter.notifyItemRangeInserted(oldCount, tvShows.size());
+                }
+            }
+        });
+    }
+
+    private void toggleLoading() {
+        if (currentPage == 1) {
+            if (activityTvshowsBinding.getIsLoading() != null && activityTvshowsBinding.getIsLoading()) {
+                activityTvshowsBinding.setIsLoading(false);
+            } else {
+                activityTvshowsBinding.setIsLoading(true);
+            }
+        } else {
+            if (activityTvshowsBinding.getIsLoadingMore() != null && activityTvshowsBinding.getIsLoadingMore()) {
+                activityTvshowsBinding.setIsLoadingMore(false);
+            } else {
+                activityTvshowsBinding.setIsLoadingMore(true);
+            }
+        }
     }
 }
---
+++ app/src/main/java/edu/northeastern/fall22_team34/adapters/TVShowsAdapter.java
@@ -0,0 +1,62 @@
+package edu.northeastern.fall22_team34.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.northeastern.fall22_team34.R;
+import edu.northeastern.fall22_team34.databinding.ItemContainerTvShowBinding;
+import edu.northeastern.fall22_team34.models.TVShow;
+import kotlin.jvm.internal.PropertyReference0Impl;
+
+public class TVShowsAdapter extends RecyclerView.Adapter<TVShowsAdapter.TVShowViewHolder> {
+
+    private List<TVShow> tvShows;
+    private LayoutInflater layoutInflater;
+
+    public TVShowsAdapter(List<TVShow> tvShows) {
+        this.tvShows = tvShows;
+    }
+
+    @NonNull
+    @Override
+    public TVShowViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater == null) {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        ItemContainerTvShowBinding tvShowBinding = DataBindingUtil.inflate(
+                layoutInflater, R.layout.item_container_tv_show, parent, false
+        );
+        return new TVShowViewHolder(tvShowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TVShowViewHolder holder, int position) {
+        holder.bindTVShow(tvShows.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return tvShows.size();
+    }
+
+    static  class TVShowViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemContainerTvShowBinding itemContainerTvShowBinding;
+
+        public TVShowViewHolder(ItemContainerTvShowBinding itemContainerTvShowBinding) {
+            super(itemContainerTvShowBinding.getRoot());
+            this.itemContainerTvShowBinding = itemContainerTvShowBinding;
+        }
+
+        public void bindTVShow(TVShow tvShow) {
+            itemContainerTvShowBinding.setTvShow(tvShow);
+            itemContainerTvShowBinding.executePendingBindings();
+        }
+    }
+}
